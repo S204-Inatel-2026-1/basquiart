f74b58952c1516bbe9aaec373579ea45dbbe7e6a
Nathalia-Costaa <nathaliaaparecida1804@gmail.com>
2026-03-19 13:54:32 -0300

    feat: Add post endpoints without implementation

---
+++ features/post/router.py
@@ -0,0 +1,83 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status, Query, Form, UploadFile, File
+
+from features.auth.utils import get_current_user
+from features.post.image_handler import image_handler
+from features.post.service import PostService
+from features.post.schema import RatePostBody, PaginatedPostsResponse
+from features.core.database import db
+
+router = APIRouter(prefix="/posts", tags=["posts"])
+service = PostService(db)
+
+
+@router.get("/{group_id}", response_model=PaginatedPostsResponse)
+async def get_posts(
+        group_id: int,
+        user_id: int = Depends(get_current_user),
+        page: int = Query(default=1, ge=1),
+        page_size: int = Query(default=10, ge=1, le=100),
+):
+    try:
+        posts = await service.get_posts(user_id, group_id, page, page_size)
+        return posts
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/{group_id}", status_code=status.HTTP_201_CREATED)
+async def make_post(
+        group_id: int,
+        content: str = Form(...),
+        images: List[UploadFile] = File(default=[]),
+        user_id: int = Depends(get_current_user),
+):
+    image_urls = []
+    try:
+        for image in images:
+            url = await image_handler.save(image)
+            image_urls.append(url)
+
+        return await service.make_post(user_id, group_id, content, image_urls)
+    except ValueError as e:
+        for url in image_urls:  # clean up any saved files if something fails
+            image_handler.delete(url)
+
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(
+        post_id: int,
+        user_id: int = Depends(get_current_user),
+):
+    try:
+        image_urls = await service.delete_post(user_id, post_id)
+        for url in image_urls:
+            image_handler.delete(url)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/{post_id}/like")
+async def toggle_like(
+    post_id: int,
+    user_id: int = Depends(get_current_user),
+):
+    try:
+        return await service.toggle_like(user_id, post_id)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/{post_id}/rate")
+async def rate_post(
+    post_id: int,
+    body: RatePostBody,
+    user_id: int = Depends(get_current_user),
+):
+    try:
+        return await service.rate_post(user_id, post_id, body.ratings)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
